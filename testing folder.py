956cda82770a766b80e0d0bd9b51d2c570c92e14
Ryheem Bonaparte <108358749+Ryheembon@users.noreply.github.com>
2022-07-14 14:09:15 -0700

    Add files via upload

---
+++ testing folder.py
@@ -0,0 +1,36 @@
+# Simple calculator:
+
+num1 = input("Enter your first number:")
+num2 = input("Enter you second number:")
+result = float(num1) + float(num2)
+print(result)
+
+# Outputting input's for name:
+
+name = input("Enter you name:")
+age = input("Enter your age:")
+print("Hello " + name +  "!" " You are " + age + " year of age")
+print = input(age)  
+
+# this is a list of fruits:
+
+real_fruit = [["apples", "large"], ["orange", "small" ], ["grapes", "large"]]
+print("real_fruit")
+
+# Appending a list:
+
+real_fruit = [["apples", "large"], ["orange", "small" ], ["grapes", "large"]]
+print (real_fruit)
+real_fruit.append("mangos"),
+print(real_fruit) 
+
+# Booleans for if it's true or false:
+
+a = 50
+b = 100
+
+if a >= b:
+    print("a is greater than or equal to b")
+else:
+    print(" a is not greater then or equal to b")
+
